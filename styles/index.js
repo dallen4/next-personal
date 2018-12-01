08fb7a861aaa331a5ce2e5df240d14ef2ab385d2
Dominique Allen <dallen4@luc.edu>
2018-11-30 20:16:05 -0600

    create index.js styles file for index page styles objects

---
+++ styles/index.js
@@ -0,0 +1,41 @@
+const styles = {
+    navContainer: {
+        width: '80%',
+        marginLeft: '10%',
+        marginRight: '10%',
+        paddingTop: '15px',
+        position: 'fixed',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    },
+    backTopButton: {
+        position: 'fixed',
+        bottom: 0,
+        right: 0,
+        marginBottom: '8px',
+        borderTopLeftRadius: '5px',
+        borderBottomLeftRadius: '5px',
+        backgroundColor: 'rgba(51,54,57,0.6)'
+    },
+    HomeSectionContentContainer: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'rgba(0.2, 0.3, 2, 0.7)',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    HomeSectionH1: {
+        color: 'rgb(22, 175, 101)'
+    },
+    HomeSectionH2: {
+        color: 'rgb(28, 109, 161)'
+    },
+    HomeSectionH3: {
+        color: 'rgb(252, 154, 31)'
+    },
+};
+
+export { styles };
