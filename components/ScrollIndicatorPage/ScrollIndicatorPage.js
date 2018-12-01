e05342e1543d1d88a7549c2bdca3bd07272ace21
Dominique Allen <dallen4@luc.edu>
2018-11-30 21:56:31 -0600

    make ScrollIndicatorPage component dynamic to go across bottom or side of screen

--- components/ScrollIndicatorPage/ScrollIndicatorPage.js
+++ components/ScrollIndicatorPage/ScrollIndicatorPage.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ScrollIndicatorPage extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            width: `0%`,
+            interpolatedValue: `0%`,
         };
 
         this.handleScroll = this.handleScroll.bind(this);
     }
+
+    static propTypes = {
+        horizontal: PropTypes.bool,
+        customStyles: PropTypes.object,
+    };
+
+    static defaultProps = {
+        horizontal: true,
+        customStyles: {},
+    };
     
     componentDidMount() {
         this.handleScroll();
     }
 
     handleScroll() {
-        let viewHeight = window.innerHeight;
+
+        // get current offset from top of document
         let heightOffset = window.pageYOffset;
-        let documentHeight = document.body.clientHeight;
-        let percentage = (heightOffset / (documentHeight - viewHeight)) * 100;
-        let width = `${Math.floor(percentage)}%`;
-        this.setState({ width });
+
+        // subtract view height from total document height
+        let docViewDiff = document.body.clientHeight - window.innerHeight;
+
+        let percentage = (heightOffset / docViewDiff * 100).toFixed(2);
+        let interpolatedValue = `${percentage}%`;
+
+        this.setState({ interpolatedValue });
+
     }
 
     render() {
-        let { width } = this.state;
+
+        let { horizontal, customStyles, } = this.props;
+        let { interpolatedValue } = this.state;
 
         let styles = {
-            width,
+            left: 0,
             bottom: 0,
             position: 'fixed',
             height: '3px',
+            width: '3px',
             backgroundColor: 'rgb(252, 154, 31)',
+            ...customStyles,
         };
+
+        let interpolatedField = horizontal ? 'width' : 'height';
+
+        styles[interpolatedField] = interpolatedValue;
 
         return (
             <div onWheel={this.handleScroll}>
                 {this.props.children}
-                <div
-                    style={styles}
-                />
+                <div style={styles} />
             </div>
         );
     }
