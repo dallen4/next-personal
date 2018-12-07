c1969fb29f6e84fd588ecc8a52ea47d5217e119c
Dominique Allen <dallen4@luc.edu>
2018-12-06 23:28:49 -0600

    create color config

---
+++ config/color.js
@@ -0,0 +1,14 @@
+// color config
+
+const colorPalette = {
+    gray: 'rgb(51, 54, 57)',
+    lightGray: '',
+    blue: 'rgb(28, 109, 161)',
+    green: 'rgb(22, 175, 101)',
+    orange: 'rgb(252, 154, 31)',
+    white: '#ffffff',
+};
+
+export {
+    colorPalette,
+};
