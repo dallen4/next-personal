a45b704ea8e13033c4d01647be2f65702300571e
Dominique Allen <dallen4@luc.edu>
2019-03-01 01:23:26 -0600

    add blog color palette

---
+++ styles/colors.js
@@ -0,0 +1,12 @@
+
+const colorPalette = {
+    blue: '#5D65BA', /* Toolbox */
+    lightBlue: '#01C3CC', /* Robin egg blue */
+    green: '#232A2E', /* Medium jungle green */
+    gray: '#868A8C', /* Taupe gray */
+    white: '#FFFFFF'
+};
+
+export {
+    colorPalette,
+};
