6beb8a2a8ee2d8d023cb0f19191ce0bc667cf329
Dominique Allen <dallen4@luc.edu>
2019-03-20 00:46:14 -0500

    handle mongoose deprecation warnings

--- api/dbms/index.js
+++ api/dbms/index.js
@@ -4,6 +4,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI).catch(error => console.log(error));
+// was throwing error, implemented to fix
+// ref: https://github.com/Automattic/mongoose/issues/6890
+mongoose.set('useCreateIndex', true);
+
+// useNewUrlParser resolves depracation warning
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+}).catch(error => console.log('failed to connect to MongoDB...'));
 
 module.exports = mongoose;
