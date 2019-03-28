da7320f0ab7fa693085ddfbbbe654a18a544eb39
Dominique Allen <dallen4@luc.edu>
2019-03-28 09:30:32 -0400

    update styling for /write, could probably be functional component

--- pages/write.js
+++ pages/write.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
-import getClient from '../lib/client/init';
 import { styles } from '../styles/write';
 import EditPane from '../components/EditPane';
-import { colorPalette } from '../styles/colors';
+import { writePalette } from '../styles/colors';
+import withAuth from '../components/withAuth';
 
-const client = getClient();
+export default class Write extends React.Component {
 
-export default () => (
-    <div style={styles.container} >
-        <style global jsx>{`
-            body {
-                background: ${colorPalette.green};
-            }
-        `}</style>
-        <h1 style={{ margin: 0, color: colorPalette.white }} >Write</h1>
-        <Link  href='/' >
-            <a style={{ color: colorPalette.lightBlue }} >go home</a>
-        </Link>
-        <EditPane/>
-    </div>
-)
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div style={styles.container} >
+                <style global jsx>{`
+                    body {
+                        background: ${writePalette.green};
+                    }
+                `}</style>
+                <h1 style={{ margin: 0, color: writePalette.white }} >Write</h1>
+                <Link  href='/' >
+                    <a style={{ color: writePalette.lightBlue }} >go home</a>
+                </Link>
+                <EditPane/>
+            </div>
+        );
+    }
+}
+
+// export default withAuth(Write);
