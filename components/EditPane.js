7aa4f7097ef96cfd07fa1d140b165cf3409ca6b7
Dominique Allen <dallen4@luc.edu>
2019-03-28 09:29:55 -0400

    implement custom Button component, consider making functional component

--- components/EditPane.js
+++ components/EditPane.js
@@ -1,42 +1,38 @@
 // Editor component
 
 import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
 import { writePalette } from '../styles/colors';
 import TextInput from '../components/TextInput';
 import TextEditor from './TextEditor/TextEditor';
 import CustomSelect from './CustomSelect';
+import Button from './Button';
 
 export default class EditPane extends Component {
 
     constructor(props) {
-
         super(props);
 
-        this.state = {
-            title: '',
-            categoryOptions: [],
-            categories: [],
-            categoryInput: '',
-            body: '',
-        };
-
-        this._onEditorChange = this._onEditorChange.bind(this);
-        this._onSave = this._onSave.bind(this);
+        this.savePost = this.savePost.bind(this);
 
     }
 
-    _onEditorChange = (body) => this.setState({ body });
+    containsErrors = () => {
+        let errors = [
+            this.titleInput.containsError(),
+            this.categorySelect.value().length === 0,
+            this.contentInput.isEmpty(),
+        ];
 
-    _onSave = (data) => {
-        // TODO
+        return errors.includes(true) ? true : false;
+    };
+
+    savePost = () => {
         console.log(this.titleInput.value());
         console.log(this.categorySelect.value());
+        console.log(this.contentInput.getHTML());
     };
 
     render() {
-
-        // TODO break out into own components
         return (
             <div style={{
                 flex: 1,
@@ -54,38 +50,16 @@ export default class EditPane extends Component {
                 <CustomSelect
                     ref={ref => this.categorySelect = ref}
                 />
-                
-                <TextEditor onContentChange={this._onEditorChange} />
 
-                <button
-                    style={{ width: '120px', padding: '10px', backgroundImage: `linear-gradient(to right, ${writePalette.lightBlue}, ${writePalette.blue})`, borderRadius: '5px', color: writePalette.white }}
-                    className='button'
-                    onClick={this._onSave}
-                >
-                    Save
-                </button>
+                <TextEditor
+                    ref={ref => this.contentInput = ref}
+                />
 
-                {/* <Mutation
-                    mutation={NEW_POST}
-                    variables={{
-                        title,
-                        categories,
-                        body
-                    }}
-                    onCompleted={this._onSave}
-                >
-                    {(mutation, { loading, error }) => {
-                        if (loading) {
-                            return <h2>loading...</h2>;
-                        } else if (error) {
-                            return <h3>{error.message}</h3>;
-                        } else
-                            return (
-                                <button onClick={mutation} className='button' >Save</button>
-                            );
-                        }
-                    }
-                </Mutation> */}
+                <Button
+                    label={'Save'}
+                    onClick={this.savePost}
+                    disabled={false}
+                />
             </div>
         );
     }
