32b4f33d97fa612534f011f3b1a38b898645b21a
Dominique Allen <dallen4@luc.edu>
2019-03-28 09:28:33 -0400

    structure /login

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Router from 'next/router';
 import LoginForm from '../components/forms/LoginForm';
+import { writePalette } from '../styles/colors';
 
 export default () => (
-    <LoginForm
-        onAuthComplete={() => Router.push('/write')}
-    />
+    <div
+        className={'fullscreen-container'}
+        style={{
+            backgroundColor: writePalette.gray,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+        }}
+    >
+        <LoginForm onAuthComplete={() => Router.push('/write')} />
+    </div>
 );
