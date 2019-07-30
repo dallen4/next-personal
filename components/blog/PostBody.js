d1bbd912c80987490a5cbeb23941d66bb17d6be5
Nieky Allen <nieky.allen@gmail.com>
2019-07-30 00:30:21 -0500

    abstract out post content body markdown parsing

---
+++ components/blog/PostBody.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Markdown from 'markdown-to-jsx';
+import Highlight from 'react-highlight.js';
+
+const PostBody = ({ body }) => {
+    return (
+        <Markdown
+            options={{
+                overrides: {
+                    div: {
+                        props: {
+                            className: 'content',
+                        },
+                    },
+                    p: {
+                        props: {
+                            className: 'body-text',
+                        },
+                    },
+                    code: {
+                        component: ({ children, ...props }) => {
+                            if (props.className)
+                                return <Highlight {...props}>{children}</Highlight>;
+                            else return <code>{children}</code>;
+                        },
+                    },
+                },
+            }}
+        >
+            {body}
+        </Markdown>
+    );
+};
+
+PostBody.propTypes = {
+    body: PropTypes.string.isRequired,
+};
+
+export default PostBody;
