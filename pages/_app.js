676aa6b7c0a97f97a68a1d5549be4edd3e51c75c
Nieky Allen <nieky.allen@gmail.com>
2019-08-06 20:58:19 -0600

    implement sidebar

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,36 @@ import React from 'react';
 import '../styles/base.css';
 import withClient from '../lib/client/withClient';
 import { ApolloProvider } from 'react-apollo';
+import Sidebar from 'react-sidebar';
+import SidebarMenu from '../components/blog/SidebarMenu';
 
 class MyApp extends App {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sidebarOpen: false,
+        };
+    }
+
+    toggleSidebar = () => this.setState({ sidebarOpen: !this.state.sidebarOpen });
+
     render() {
         const { Component, pageProps, client } = this.props;
         return (
             <Container>
                 <ApolloProvider client={client} >
-                    <Component {...pageProps} />
+                        <Sidebar
+                            sidebarClassName={'sidebar'}
+                            sidebar={<SidebarMenu toggleSidebar={this.toggleSidebar} />}
+                            open={this.state.sidebarOpen}
+                            onSetOpen={this.toggleSidebar}
+                        >
+                            <Component
+                                {...pageProps}
+                                toggleSidebar={this.toggleSidebar}
+                            />
+                        </Sidebar>
                 </ApolloProvider>
             </Container>
         );
