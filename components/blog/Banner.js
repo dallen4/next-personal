944f026830eb82717f69a5d6f6fc318ae91d5eab
Nieky Allen <nieky.allen@gmail.com>
2019-08-13 11:33:54 -0500

    implement query, make more responsive

--- components/blog/Banner.js
+++ components/blog/Banner.js
@@ -2,11 +2,23 @@ import React from 'react';
 import Router from 'next/router';
 import Button from './Button';
 import CategoryLink from './CategoryLink';
-import Header from './Header';
+import Header, { MobileHeader } from './Header';
+import { Query } from 'react-apollo';
+import { LIST_POSTS } from '../../lib/queries';
+import LoadingView from '../LoadingView';
 
-let content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const postPlaceholder = {
+    title: 'Welcome!',
+    description: 'This is a placeholder post for the featured banner.',
+    slug: '',
+    publishedAt: Date.now(),
+    tag: {
+        name: 'general',
+    },
+    body: 'Thanks for checking out my site! There will be posts up soon.',
+};
 
-export default function Banner() {
+export default function Banner({ toggleSidebar }) {
     return (
         <div className={'banner'}>
             <style jsx>{`
@@ -14,95 +26,141 @@ export default function Banner() {
                     color: white;
                 }
 
-                h3, p {
-                    color: rgb(129,129,129);
+                h3,
+                p {
+                    color: rgb(129, 129, 129);
                 }
 
                 h3 {
                     font-weight: normal;
                 }
             `}</style>
-            <div
-                className={'home-banner'}
-                style={{
-                    height: '100%',
-                    borderBottomRightRadius: '45px',
-                    overflow: 'hidden',
-                    background: `url('https://images.unsplash.com/photo-1494522855154-9297ac14b55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')`,
-                }}
+            <Query
+                query={LIST_POSTS}
+                notifyOnNetworkStatusChange
+                fetchPolicy={'cache-first'}
             >
-                <div
-                    style={{
-                        height: '100%',
-                        width: '100%',
-                        background:
-                            'linear-gradient(to right, transparent, 25%, rgb(40,42,46))',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'flex-start',
-                    }}
-                >
-                    <Header/>
-                    <div
-                        style={{
-                            flex: 1,
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'flex-end',
-                        }}
-                    >
-                        <div
-                            className={'banner-content'}
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'column',
-                                justifyContent: 'space-evenly',
-                                alignItems: 'flex-start',
-                            }}
-                        >
-                            <h1 className={'post-title'} >
-                                Using Strapi to Streamline Your Development Time
-                            </h1>
-                            <h3
+                {({ data, loading, error, networkStatus }) => {
+                    if (loading || networkStatus === 4 || error)
+                        return (
+                            <LoadingView
+                                fullscreen={false}
+                                loading={loading}
+                                error={error}
+                                errorMsg={'Issue fetching latest post...'}
+                            />
+                        );
+                    else {
+
+                        let featuredPost = postPlaceholder;
+
+                        if (data.posts.length > 0)
+                            featuredPost = data.posts[0];
+
+                        let {
+                            title,
+                            description,
+                            slug,
+                            imageUrl,
+                            publishedAt,
+                            tag: { name: tagName },
+                            body,
+                        } = featuredPost;
+
+                        let content = `${body.substr(0, 280)}...`;
+
+                        return (
+                            <div
+                                className={'home-banner'}
                                 style={{
-                                    color: 'rgba(255,255,255,0.7)',
+                                    height: '100%',
+                                    borderBottomRightRadius: '45px',
+                                    overflow: 'hidden',
+                                    background: `url('${
+                                        imageUrl ||
+                                        'https://images.unsplash.com/photo-1494522855154-9297ac14b55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'
+                                    }')`,
                                 }}
                             >
-                                Breaking down your development timeline without reinventing the wheel.
-                            </h3>
-                            <div>
-                                <p
+                                <div
                                     style={{
-                                        display: 'inline',
+                                        height: '100%',
+                                        width: '100%',
+                                        background:
+                                            'linear-gradient(to right, transparent, 25%, rgb(40,42,46))',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        justifyContent: 'flex-start',
                                     }}
                                 >
-                                    {new Date('2019-07-06T02:33:23.940Z').toLocaleDateString('en-US', {
-                                        month: 'long',
-                                        day: 'numeric',
-                                        year: 'numeric',
-                                    })} &bull; In{' '}
-                                </p>
-                                <CategoryLink
-                                    category={'music'}
-                                />
+                                    <Header />
+                                    <MobileHeader
+                                        toggleSidebar={toggleSidebar}
+                                    />
+                                    <div
+                                        style={{
+                                            flex: 1,
+                                            display: 'flex',
+                                            flexDirection: 'row',
+                                            justifyContent: 'flex-end',
+                                        }}
+                                    >
+                                        <div
+                                            className={'banner-content'}
+                                            style={{
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                                justifyContent: 'space-evenly',
+                                                alignItems: 'flex-start',
+                                            }}
+                                        >
+                                            <h1 className={'post-title'}>
+                                                {title}
+                                            </h1>
+                                            <h3
+                                                style={{
+                                                    color: 'rgba(255,255,255,0.7)',
+                                                }}
+                                            >
+                                                {description}
+                                            </h3>
+                                            <div>
+                                                <p
+                                                    style={{
+                                                        display: 'inline',
+                                                    }}
+                                                >
+                                                    {new Date(publishedAt).toLocaleDateString('en-US', {
+                                                        month: 'long',
+                                                        day: 'numeric',
+                                                        year: 'numeric',
+                                                    })}{' '}
+                                                    &bull; In{' '}
+                                                </p>
+                                                <CategoryLink category={tagName} />
+                                            </div>
+                                            <p
+                                                className={'banner-post-body'}
+                                                style={{
+                                                    color: 'rgba(255,255,255,0.7)',
+                                                    lineHeight: '1.5rem',
+                                                }}
+                                            >
+                                                {content}
+                                            </p>
+                                            <Button
+                                                label={'Read More'}
+                                                color={'orange'}
+                                                onClick={() => Router.push(`/post/${slug}`)}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <p
-                                style={{
-                                    color: 'rgba(255,255,255,0.7)',
-                                    lineHeight: '1.5rem',
-                                }}
-                            >
-                                {`${content.substr(0, 240)}...`}
-                            </p>
-                            <Button
-                                label={'Read More'}
-                                color={'orange'}
-                                onClick={() => Router.push('/')}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        );
+                    }
+                }}
+            </Query>
         </div>
     );
 }
